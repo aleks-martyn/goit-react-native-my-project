ece4b353b154577484dd9f64c9539278d3506ab4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-02 01:27:56 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -1,23 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from "expo-font";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import RegistrationScreen from './src/Screens/RegistrationScreen/RegistrationScreen';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
-    "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
+  
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <RegistrationScreen>
+        <Text style={styles.text}>
+          Open up App.js to start working on your app!
+        </Text>
+        <StatusBar style="auto" />
+      </RegistrationScreen>
     </View>
   );
 }
@@ -25,13 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    color: "blue",
-    fontFamily: 'Roboto-Medium',
-    fontSize: 20,
-  },
+  
 });
