f67f76dc8183fef5fa6800c21e973160bd415510
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-03 01:29:46 +0300

    Update RegistrationScreen.js

--- goit-react-native-my-project/src/Screens/RegistrationScreen/RegistrationScreen.js
+++ goit-react-native-my-project/src/Screens/RegistrationScreen/RegistrationScreen.js
@@ -1,6 +1,7 @@
 import {
   Button,
   ImageBackground,
+  Image,
   SafeAreaView,
   StyleSheet,
   Text,
@@ -9,6 +10,7 @@ import {
   View,
 } from 'react-native';
 import BgImage from '../../images/PhotoBG2.jpg';
+import UnionIcon from '../../images/Union.png';
 import { useState } from 'react';
 
 export default function RegistrationScreen() {
@@ -19,7 +21,13 @@ export default function RegistrationScreen() {
     <View>
       <ImageBackground source={BgImage} resizeMode="cover" style={styles.image}>
         <View style={styles.wrap}>
-          <View style={styles.userPicture}></View>
+          <View style={styles.userPicture}>
+            <TouchableOpacity>
+              <View style={styles.wrapUnion}>
+                <Image source={UnionIcon} style={{ width: 13, height: 13 }} />
+              </View>
+            </TouchableOpacity>
+          </View>
           <Text style={styles.title}>Реєстрація</Text>
           <SafeAreaView>
             <TextInput
@@ -76,6 +84,20 @@ const styles = StyleSheet.create({
     height: 120,
     backgroundColor: '#f6f6f6',
     borderRadius: 16,
+  },
+  wrapUnion: {
+    position: 'absolute',
+    top: 81,
+    right: -12,
+    width: 25,
+    height: 25,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#ff6c00',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: '50%',
   },
   title: {
     marginBottom: 15,
