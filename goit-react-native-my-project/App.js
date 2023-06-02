b5c61368229b392abe60230983b0eabee0335184
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-02 14:39:15 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -1,19 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import RegistrationScreen from './src/Screens/RegistrationScreen/RegistrationScreen';
+import BgImage from '../goit-react-native-my-project/src/images/PhotoBG2.jpg';
 
 export default function App() {
   
 
   return (
     <View style={styles.container}>
-      <RegistrationScreen>
+      <ImageBackground source={BgImage} resizeMode="cover" style={styles.image}>
         <Text style={styles.text}>
           Open up App.js to start working on your app!
         </Text>
         <StatusBar style="auto" />
-      </RegistrationScreen>
+      </ImageBackground>
     </View>
   );
 }
@@ -21,9 +22,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
+  image: {
+    width: 320,
+    flex: 1,
+    justifyContent: 'center',
+  },
 });
