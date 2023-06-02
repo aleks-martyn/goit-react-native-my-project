8cfd52c116770a7771f1c52c9dde65913c2f5bbe
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-02 15:40:02 +0300

    Update RegistrationScreen.js

--- goit-react-native-my-project/src/Screens/RegistrationScreen/RegistrationScreen.js
+++ goit-react-native-my-project/src/Screens/RegistrationScreen/RegistrationScreen.js
@@ -1,23 +1,30 @@
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import BgImage from '../../images/PhotoBG2.jpg';
 
 export default function RegistrationScreen() {
-    console.log('screen')
-    const image = { uri: 'https://reactjs.org/logo-og.png' }
-    console.log(image)
   return (
     <View>
       <ImageBackground
-              source={image}
+        source={BgImage}
         resizeMode="cover"
         style={styles.image}
-      />
+      >
+        <View style={styles.wrap}></View>
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   image: {
+    width: 320,
     flex: 1,
     justifyContent: 'center',
   },
+  wrap: {
+    marginTop: 218,
+    width: '100%',
+    height: 350,
+    backgroundColor: '#fff',
+  }
 });
