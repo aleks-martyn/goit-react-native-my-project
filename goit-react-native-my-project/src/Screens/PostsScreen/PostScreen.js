77761cc9c79d0f4d514d557e44fe46ea6d78a19a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-03 21:20:37 +0300

    Update PostScreen.js

--- goit-react-native-my-project/src/Screens/PostsScreen/PostScreen.js
+++ goit-react-native-my-project/src/Screens/PostsScreen/PostScreen.js
@@ -1,13 +1,52 @@
-import { View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function PostScreen() {
-  return <View style={styles.wrapper}></View>;
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Публікації</Text>
+      </View>
+      <View style={styles.footer}></View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+  header: {
+    width: 320,
+    height: 88,
+    paddingBottom: 11,
+    backgroundColor: '#fff',
+    shadowOffset: {
+      width: 0,
+      height: 0.5,
+    },
+    shadowOpacity: 0.3,
+    shadowColor: '#000',
+    shadowRadius: 0,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  footer: {
+    width: 320,
+    height: 83,
+    backgroundColor: '#fff',
+    shadowOffset: {
+      width: 0,
+      height: -0.5,
+    },
+    shadowOpacity: 0.3,
+    shadowColor: '#000',
+    shadowRadius: 0,
+  },
+  title: {
+      fontSize: 17,
+      lineHeight: 22,
+      color: '#212121',
   },
 });
