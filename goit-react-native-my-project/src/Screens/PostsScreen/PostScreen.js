6104cbb1af0588ef91c93109e7b338c6533f6181
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-04 00:14:10 +0300

    Update PostScreen.js

--- goit-react-native-my-project/src/Screens/PostsScreen/PostScreen.js
+++ goit-react-native-my-project/src/Screens/PostsScreen/PostScreen.js
@@ -13,12 +13,13 @@ export default function PostScreen() {
 
 const styles = StyleSheet.create({
   wrapper: {
+    width: '100%',
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-between',
   },
   header: {
-    width: 320,
+    width: '100%',
     height: 88,
     paddingBottom: 11,
     backgroundColor: '#fff',
@@ -33,7 +34,7 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
   footer: {
-    width: 320,
+    width: '100%',
     height: 83,
     backgroundColor: '#fff',
     shadowOffset: {
@@ -45,8 +46,8 @@ const styles = StyleSheet.create({
     shadowRadius: 0,
   },
   title: {
-      fontSize: 17,
-      lineHeight: 22,
-      color: '#212121',
+    fontSize: 17,
+    lineHeight: 22,
+    color: '#212121',
   },
 });
