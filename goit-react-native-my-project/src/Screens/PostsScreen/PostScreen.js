28883b6555823505d0119e0b868cb73c5f5efbbc
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-03 19:30:33 +0300

    Update PostScreen.js

---
+++ goit-react-native-my-project/src/Screens/PostsScreen/PostScreen.js
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+
+export default function PostScreen() {
+  return <View style={styles.wrapper}></View>;
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
