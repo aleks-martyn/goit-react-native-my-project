65c2fad7121278ace46974e6dc0a575fd729f561
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-04 18:34:39 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -9,9 +9,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <LoginScreen>
+      <RegistrationScreen>
         <StatusBar style="auto" />
-      </LoginScreen>
+      </RegistrationScreen>
     </View>
   );
 }
