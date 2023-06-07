1619af5cba792d2fa7090d6b48664c3415963b3f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 22:06:06 +0300

    Update Home.js

--- goit-react-native-my-project/src/Screens/Home/Home.js
+++ goit-react-native-my-project/src/Screens/Home/Home.js
@@ -1,7 +1,7 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 export default function Home() {
-  return <View style={styles.container}></View>;
+  return <View style={styles.container}><Text>Home</Text></View>;
 }
 
 const styles = StyleSheet.create({
