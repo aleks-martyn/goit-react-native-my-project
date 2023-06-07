a58b7c7d467d63cc74edc6e676caca83caf8c3a3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 20:36:56 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -1,18 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import RegistrationScreen from './src/Screens/RegistrationScreen/RegistrationScreen';
 import LoginScreen from './src/Screens/LoginScreen/LoginScreen';
 import PostScreen from './src/Screens/PostsScreen/PostScreen';
 
 export default function App() {
-
   return (
-    <View style={styles.container}>
-      <RegistrationScreen>
-        <StatusBar style="auto" />
-      </RegistrationScreen>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <RegistrationScreen>
+          <StatusBar style="auto" />
+        </RegistrationScreen>
+      </View>
+    </NavigationContainer>
   );
 }
 
