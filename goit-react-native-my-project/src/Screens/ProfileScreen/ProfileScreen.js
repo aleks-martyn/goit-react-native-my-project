2d88e5cc3c154412932be720b52873a0fb5faff4
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 22:31:54 +0300

    Update ProfileScreen.js

---
+++ goit-react-native-my-project/src/Screens/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, View, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function ProfileScreen() {
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Text>Profile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
