00be215cad5ca41a1a59958689c76ee85181d009
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-07 21:00:54 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -2,10 +2,18 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
 import { useFonts } from 'expo-font';
 import RegistrationScreen from './src/Screens/RegistrationScreen/RegistrationScreen';
 import LoginScreen from './src/Screens/LoginScreen/LoginScreen';
-import PostScreen from './src/Screens/PostsScreen/PostScreen';
+import PostsScreen from './src/Screens/PostsScreen/PostsScreen';
+import Home from './src/Screens/Home/Home';
+import MapScreen from './src/Screens/MapScreen/MapScreen';
+import ProfileScreen from './src/Screens/ProfileScreen/ProfileScreen';
+import CreatePostsScreen from './src/Screens/CreatePostsScreen/CreatePostsScreen';
+import CommentsScreen from './src/Screens/CommentsScreen/CommentsScreen';
+
+const MainStack = createStackNavigator();
 
 export default function App() {
   return (
