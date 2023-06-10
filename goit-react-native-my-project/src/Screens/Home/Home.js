5b6e2a8b76a0dc940bdc3a84286496493011250d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-10 16:30:48 +0300

    Update Home.js

--- goit-react-native-my-project/src/Screens/Home/Home.js
+++ goit-react-native-my-project/src/Screens/Home/Home.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import PostsScreen from './src/Screens/PostsScreen/PostsScreen';
 import ProfileScreen from './src/Screens/ProfileScreen/ProfileScreen';
 import CreatePostsScreen from './src/Screens/CreatePostsScreen/CreatePostsScreen';
@@ -12,7 +12,9 @@ export default function Home() {
 
   return (
     <Tab.Navigator>
-      <Text>Home</Text>
+      <Tab.Screen name="Posts" component={PostsScreen}></Tab.Screen>
+      <Tab.Screen name="CreatePosts" component={CreatePostsScreen}></Tab.Screen>
+      <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
     </Tab.Navigator>
   );
 }
