6bd470b1f301f20b5a44aa38210276ac9d364529
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-10 16:49:25 +0300

    Update Home.js

--- goit-react-native-my-project/src/Screens/Home/Home.js
+++ goit-react-native-my-project/src/Screens/Home/Home.js
@@ -1,9 +1,9 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import PostsScreen from './src/Screens/PostsScreen/PostsScreen';
-import ProfileScreen from './src/Screens/ProfileScreen/ProfileScreen';
-import CreatePostsScreen from './src/Screens/CreatePostsScreen/CreatePostsScreen';
+import PostsScreen from '../PostsScreen/PostsScreen';
+import ProfileScreen from '../ProfileScreen/ProfileScreen';
+import CreatePostsScreen from '../CreatePostsScreen/CreatePostsScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +11,7 @@ export default function Home() {
   const navigation = useNavigation();
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Posts" component={PostsScreen}></Tab.Screen>
       <Tab.Screen name="CreatePosts" component={CreatePostsScreen}></Tab.Screen>
       <Tab.Screen name="Profile" component={ProfileScreen}></Tab.Screen>
