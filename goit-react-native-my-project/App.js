9d2df5892c53d2c350b396db8e74da321b2a1798
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-10 16:24:28 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -1,15 +1,11 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
 import RegistrationScreen from './src/Screens/RegistrationScreen/RegistrationScreen';
 import LoginScreen from './src/Screens/LoginScreen/LoginScreen';
-import PostsScreen from './src/Screens/PostsScreen/PostsScreen';
 import Home from './src/Screens/Home/Home';
 import MapScreen from './src/Screens/MapScreen/MapScreen';
-import ProfileScreen from './src/Screens/ProfileScreen/ProfileScreen';
-import CreatePostsScreen from './src/Screens/CreatePostsScreen/CreatePostsScreen';
 import CommentsScreen from './src/Screens/CommentsScreen/CommentsScreen';
 
 const MainStack = createStackNavigator();
@@ -23,12 +19,7 @@ export default function App() {
       >
         <MainStack.Screen name="Registration" component={RegistrationScreen} />
         <MainStack.Screen name="Login" component={LoginScreen} />
-        <MainStack.Group initialRouteName="Posts">
-          <MainStack.Screen name="Home" component={Home} />
-          <MainStack.Screen name="Posts" component={PostsScreen} />
-          <MainStack.Screen name="CreatePosts" component={CreatePostsScreen} />
-          <MainStack.Screen name="Profile" component={ProfileScreen} />
-        </MainStack.Group>
+        <MainStack.Screen name="Home" component={Home} />
         <MainStack.Screen name="Comments" component={CommentsScreen} />
         <MainStack.Screen name="Map" component={MapScreen} />
       </MainStack.Navigator>
