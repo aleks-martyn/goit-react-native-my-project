8166ccbb678a56be8de01db516b439c5bef15c4e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-10 19:08:56 +0300

    Update ProfileScreen.js

--- goit-react-native-my-project/src/Screens/ProfileScreen/ProfileScreen.js
+++ goit-react-native-my-project/src/Screens/ProfileScreen/ProfileScreen.js
@@ -1,12 +1,17 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, ImageBackground } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import BgImage from '../../images/PhotoBG2.jpg';
 
 export default function ProfileScreen() {
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
+      <ImageBackground
+        source={BgImage}
+        resizeMode="cover"
+        style={styles.image}
+      ></ImageBackground>
     </View>
   );
 }
@@ -18,5 +23,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
   },
 });
