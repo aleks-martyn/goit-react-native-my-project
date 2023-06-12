2d86dd51dfd4be62b65d47f7b8d2e585d7cce43e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-13 01:19:32 +0300

    Update CreatePostsScreen.js

--- goit-react-native-my-project/src/Screens/CreatePostsScreen/CreatePostsScreen.js
+++ goit-react-native-my-project/src/Screens/CreatePostsScreen/CreatePostsScreen.js
@@ -1,15 +1,19 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Camera } from "expo-camera";
-import * as Location from "expo-location";
+import React, { useState, useEffect, useRef } from 'react';
+import { Camera } from 'expo-camera';
+import * as Location from 'expo-location';
 import { StyleSheet, View, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 export default function CreatePostsScreen() {
   const navigation = useNavigation();
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
   return (
     <View style={styles.container}>
-      <Text>CreatePosts</Text>
+      <Camera type={type} ref={setCameraRef}>
+        <View></View>
+      </Camera>
     </View>
   );
 }
