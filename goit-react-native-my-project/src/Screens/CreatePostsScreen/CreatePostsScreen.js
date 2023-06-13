7a998ea3811a38257b36f1aa409f797b5e732c26
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-13 16:00:55 +0300

    Update CreatePostsScreen.js

--- goit-react-native-my-project/src/Screens/CreatePostsScreen/CreatePostsScreen.js
+++ goit-react-native-my-project/src/Screens/CreatePostsScreen/CreatePostsScreen.js
@@ -3,19 +3,44 @@ import { Camera } from 'expo-camera';
 import * as Location from 'expo-location';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Ionicons, FontAwesome } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function CreatePostsScreen() {
   const navigation = useNavigation();
   const [cameraRef, setCameraRef] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
+  const onPublish = () => {
+    console.log('Опубліковано');
+  };
+
   return (
     <View style={styles.container}>
-      <Camera type={type} ref={setCameraRef}>
-        <View>
-          <TouchableOpacity></TouchableOpacity>
+      <Camera type={type} ref={setCameraRef} style={styles.camera}>
+        <View style={styles.photoView}>
+          <TouchableOpacity style={styles.takePhotoWrap}>
+            <FontAwesome name="camera" size={24} color="#bdbdbd" />
+          </TouchableOpacity>
         </View>
       </Camera>
+
+      <Text>Завантажте фото</Text>
+
+      <TextInput
+        style={styles.nameInput}
+        placeholder="Назва..."
+        placeholderTextColor="#bdbdbd"
+      />
+      <TextInput
+        style={styles.locationInput}
+        placeholder="Місцевість..."
+        placeholderTextColor="#bdbdbd"
+      />
+
+      <TouchableOpacity style={styles.publishBtn} onPress={onPublish}>
+        <Text style={styles.textPublishBtn}>Опублікувати</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -25,7 +50,60 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 10,
+    backgroundColor: '#fff',
+  },
+  camera: {
+    borderRadius: 8,
+    height: 200,
+    marginBottom: 8,
+  },
+  photoView: {
+    flex: 1,
+    width: 288,
+    height: 200,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f6f6f6',
+    borderRadius: 8,
+    borderColor: '#e8e8e8',
+    borderWidth: 1,
+    borderStyle: 'solid',
+  },
+  takePhotoWrap: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nameInput: {
+    marginTop: 10,
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bdbdbd',
+  },
+  locationInput: {
+    marginTop: 10,
+    paddingBottom: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bdbdbd',
+  },
+  publishBtn: {
+    height: 40,
+    marginTop: 25,
+    marginBottom: 16,
+    alignItems: 'center',
+    backgroundColor: '#f6f6f6',
+    paddingVertical: 10,
+    paddingHorizontal: 80,
+    borderRadius: 100,
+  },
+  textPublishBtn: {
+    color: '#bdbdbd',
+    fontSize: 16,
+    lineHeight: 19,
   },
 });
