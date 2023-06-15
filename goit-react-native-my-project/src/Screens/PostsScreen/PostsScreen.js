646bff342392f36692a27a42a04ff2358642d06d
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-15 20:54:09 +0300

    Update PostsScreen.js

--- goit-react-native-my-project/src/Screens/PostsScreen/PostsScreen.js
+++ goit-react-native-my-project/src/Screens/PostsScreen/PostsScreen.js
@@ -1,10 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 
 export default function PostsScreen() {
   const navigation = useNavigation();
+  const route = useRoute();
   const [posts, setPosts] = useState([]);
+
+  console.log(posts);
+
+  useEffect(() => {
+    if (route.params && route.params.post) {
+      setPosts(prev => [route.params.post, ...prev]);
+    }
+  }, [route.params]);
 
   return <View style={styles.container}></View>;
 }
