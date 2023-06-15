b6083b760a6d1a1adedaa8bf9b89a7221b907a16
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-15 16:44:25 +0300

    Update PostsScreen.js

--- goit-react-native-my-project/src/Screens/PostsScreen/PostsScreen.js
+++ goit-react-native-my-project/src/Screens/PostsScreen/PostsScreen.js
@@ -1,6 +1,9 @@
+import React, { useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 
 export default function PostsScreen() {
+  const [posts, setPosts] = useState([]);
+
   return <View style={styles.container}></View>;
 }
 
