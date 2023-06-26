99e572c9f71543a4d71f01890b49182cadefdc90
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-26 18:48:49 +0300

    Create userStack.js

---
+++ goit-react-native-my-project/navigation/userStack.js
@@ -0,0 +1,37 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from '../src/Screens/Home/Home';
+import MapScreen from '../src/Screens/MapScreen/MapScreen';
+import CommentsScreen from '../src/Screens/CommentsScreen/CommentsScreen';
+
+const MainStack = createStackNavigator();
+
+export default function UserStack() {
+  return (
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <MainStack.Screen name="Home" component={Home} />
+        <MainStack.Screen
+          name="Comments"
+          component={CommentsScreen}
+          options={{
+            headerShown: true,
+            title: 'Коментарі',
+            headerBackTitleVisible: false,
+            headerTintColor: '#212121',
+          }}
+        />
+        <MainStack.Screen
+          name="Map"
+          component={MapScreen}
+          options={{
+            headerShown: true,
+            title: 'Мапа',
+            headerBackTitleVisible: false,
+            headerTintColor: '#212121',
+          }}
+        />
+      </MainStack.Navigator>
+    </NavigationContainer>
+  );
+}
