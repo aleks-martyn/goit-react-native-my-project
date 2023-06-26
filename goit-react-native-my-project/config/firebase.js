78b738953337cfadffe90858042e4eac63201a1b
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-26 18:49:11 +0300

    Update firebase.js

--- goit-react-native-my-project/config/firebase.js
+++ goit-react-native-my-project/config/firebase.js
@@ -1,8 +1,5 @@
 import { initializeApp } from 'firebase/app';
 import Constants from 'expo-constants';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: Constants.manifest?.extra?.firebaseApiKey,
@@ -16,6 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+export default app;
