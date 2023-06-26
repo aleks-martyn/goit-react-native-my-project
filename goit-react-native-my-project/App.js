8b40a5deb92bbfb259fdd51b167210d8747d2c8e
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-26 18:49:16 +0300

    Update App.js

--- goit-react-native-my-project/App.js
+++ goit-react-native-my-project/App.js
@@ -1,53 +1,8 @@
 import 'react-native-gesture-handler';
-import {
-  NavigationContainer,
-  useNavigationContainerRef,
-} from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { useFonts } from 'expo-font';
-import { TouchableOpacity } from 'react-native';
-import { Feather } from '@expo/vector-icons';
-import RegistrationScreen from './src/Screens/RegistrationScreen/RegistrationScreen';
-import LoginScreen from './src/Screens/LoginScreen/LoginScreen';
-import Home from './src/Screens/Home/Home';
-import MapScreen from './src/Screens/MapScreen/MapScreen';
-import CommentsScreen from './src/Screens/CommentsScreen/CommentsScreen';
-
-const MainStack = createStackNavigator();
+import './config/firebase';
+import RootNavigation from './navigation';
 
 export default function App() {
-  //const navigationRef = useNavigationContainerRef();
-
-  return (
-    <NavigationContainer>
-      <MainStack.Navigator
-        initialRouteName="Login"
-        screenOptions={{ headerShown: false }}
-      >
-        <MainStack.Screen name="Registration" component={RegistrationScreen} />
-        <MainStack.Screen name="Login" component={LoginScreen} />
-        <MainStack.Screen name="Home" component={Home} />
-        <MainStack.Screen
-          name="Comments"
-          component={CommentsScreen}
-          options={{
-            headerShown: true,
-            title: 'Коментарі',
-            headerBackTitleVisible: false,
-            headerTintColor: '#212121',
-          }}
-        />
-        <MainStack.Screen
-          name="Map"
-          component={MapScreen}
-          options={{
-            headerShown: true,
-            title: 'Мапа',
-            headerBackTitleVisible: false,
-            headerTintColor: '#212121',
-          }}
-        />
-      </MainStack.Navigator>
-    </NavigationContainer>
-  );
+  return <RootNavigation />;
 }
