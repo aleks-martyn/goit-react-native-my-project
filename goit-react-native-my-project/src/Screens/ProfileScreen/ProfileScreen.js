f1fbc0792a5694f46b6afa406f09dc008fa9ce80
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-27 23:24:51 +0300

    Update ProfileScreen.js

--- goit-react-native-my-project/src/Screens/ProfileScreen/ProfileScreen.js
+++ goit-react-native-my-project/src/Screens/ProfileScreen/ProfileScreen.js
@@ -1,17 +1,21 @@
 import { StyleSheet, View, Text, ImageBackground } from 'react-native';
+import { getAuth } from 'firebase/auth';
 import { useNavigation } from '@react-navigation/native';
 import BgImage from '../../images/PhotoBG2.jpg';
 
 export default function ProfileScreen() {
   const navigation = useNavigation();
+  const auth = getAuth();
+  const user = auth.currentUser;
 
   return (
     <View style={styles.container}>
-      <ImageBackground
-        source={BgImage}
-        resizeMode="cover"
-        style={styles.image}
-      ></ImageBackground>
+      <ImageBackground source={BgImage} resizeMode="cover" style={styles.image}>
+        <View style={styles.userWrap}>
+          <Text>{user.email}</Text>
+          <Text>{ user.displayName ?? 'underfined'}</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -29,6 +33,13 @@ const styles = StyleSheet.create({
     height: '100%',
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  userWrap: {
+    width: 150,
+    height: 40,
+    padding: 5,
+    backgroundColor: '#fff',
   },
 });
