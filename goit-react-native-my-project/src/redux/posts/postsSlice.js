bd3a12bd273e16bed74322cc0531a80fa196d097
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-29 00:30:59 +0300

    Update postsSlice.js

--- goit-react-native-my-project/src/redux/posts/postsSlice.js
+++ goit-react-native-my-project/src/redux/posts/postsSlice.js
@@ -1,25 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addPost, getAllPosts } from './postsOperations';
+import { getAllPosts } from './postsOperations';
 
 const initialState = {
   posts: [],
 };
 
 const handleFulfilledGetAllPosts = (state, { payload }) => {
-  state.posts = payload;
-};
-
-const handleFulfilledAddPost = (state, { payload }) => {
-  state.posts = [payload, ...state.posts];
+    state.posts = payload;
+    console.log(`payload: ${payload}`)
 };
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   extraReducers: builder => {
-    builder
-      .addCase(getAllPosts.fulfilled, handleFulfilledGetAllPosts)
-      .addCase(addPost.fulfilled, handleFulfilledAddPost);
+    builder.addCase(getAllPosts.fulfilled, handleFulfilledGetAllPosts);
   },
 });
 
