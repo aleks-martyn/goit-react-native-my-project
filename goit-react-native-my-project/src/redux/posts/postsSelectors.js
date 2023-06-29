bb7b82b9be9aceb069566b47b4a7a031977a022c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-29 17:29:42 +0300

    Update postsSelectors.js

--- goit-react-native-my-project/src/redux/posts/postsSelectors.js
+++ goit-react-native-my-project/src/redux/posts/postsSelectors.js
@@ -1,3 +1,11 @@
 import { createSelector } from '@reduxjs/toolkit';
+import { selectUserId } from '../auth/authSelectors';
 
-export const selectPosts = state => state.posts;
+export const selectPosts = state => state.posts.posts;
+
+export const selectUserPosts = createSelector(
+  [selectPosts, selectUserId],
+  (posts, uid) => {
+    return posts.filter(post => post.uid === uid);
+  }
+);
