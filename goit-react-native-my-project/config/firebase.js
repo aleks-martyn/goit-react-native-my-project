cb5436a4002558b843c48d79a5c2d6b45b40dd94
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-07-02 21:14:56 +0300

    Update firebase.js

--- goit-react-native-my-project/config/firebase.js
+++ goit-react-native-my-project/config/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
-import Constants from 'expo-constants';
+//import Constants from 'expo-constants';
 
 const firebaseConfig = {
-  apiKey: Constants.manifest?.extra?.firebaseApiKey,
-  authDomain: Constants.manifest?.extra?.firebaseAuthDomain,
-  projectId: Constants.manifest?.extra?.firebaseProjectId,
-  storageBucket: Constants.manifest?.extra?.firebaseStorageBucket,
-  messagingSenderId: Constants.manifest?.extra?.firebaseMessagingSenderId,
-  appId: Constants.manifest?.extra?.firebaseAppId,
-  measurementId: Constants.manifest?.extra?.firebaseMeasurementId,
+  apiKey: 'AIzaSyCqBUoPniFFbwK17TCKYMpmhNMnVOeiLws',
+  authDomain:'posts-9d0f3.firebaseapp.com',
+  projectId: 'posts-9d0f3',
+  storageBucket: 'posts-9d0f3.appspot.com',
+  messagingSenderId: '1098182767392',
+  appId: '1:1098182767392:web:2b10c6a554b841586b9d45',
+  measurementId: 'G-3C9KNLM62Q',
 };
 
 const app = initializeApp(firebaseConfig);
