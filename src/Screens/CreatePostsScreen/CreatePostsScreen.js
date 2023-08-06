512c424a246b901cc248667e0f046544322d24f3
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-06 16:16:02 +0300

    Create CreatePostsScreen.js

---
+++ src/Screens/CreatePostsScreen/CreatePostsScreen.js
@@ -0,0 +1,240 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+import * as Location from 'expo-location';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather, FontAwesome, MaterialIcons } from '@expo/vector-icons';
+import { TextInput } from 'react-native-gesture-handler';
+import { addPost } from '../../redux/posts/postsOperations';
+import { getAuth } from 'firebase/auth';
+
+export default function CreatePostsScreen() {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [name, setName] = useState('');
+  const [nameLocation, setNameLocation] = useState('');
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [photo, setPhoto] = useState(null);
+
+  const pablishBtnIsActive = photo
+    ? styles.publishBtnIsActive
+    : styles.publishBtn;
+  const textPublishBtnIsActive = photo
+    ? styles.textPublishBtnIsActive
+    : styles.textPublishBtn;
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === 'granted');
+
+      let locationPermission =
+        await Location.requestForegroundPermissionsAsync();
+      if (locationPermission.status !== 'granted') {
+        console.log('Permission to access location was denied');
+      }
+    })();
+  }, []);
+
+  if (hasPermission === false || null) {
+    return <Text>No access to camera</Text>;
+  }
+
+  const takePhoto = async () => {
+    if (cameraRef) {
+      const { uri } = await cameraRef.takePictureAsync();
+      await MediaLibrary.createAssetAsync(uri);
+      setPhoto(uri);
+    }
+  };
+
+  const flipCamera = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  const onPublish = async () => {
+    if (!photo) return;
+
+    try {
+      let location = await Location.getCurrentPositionAsync({});
+      const { uid, displayName } = getAuth().currentUser;
+      const post = { uid, photo, location, name, nameLocation, displayName };
+      await dispatch(addPost(post)).unwrap();
+      navigation.navigate('Posts', { post });
+      setName('');
+      setNameLocation('');
+      setPhoto(null);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Camera type={type} ref={setCameraRef} style={styles.camera}>
+          <View style={styles.photoView}>
+            <TouchableOpacity style={styles.flipContainer} onPress={flipCamera}>
+              <MaterialIcons
+                name="flip-camera-android"
+                size={24}
+                color="#fff"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.takePhotoWrap} onPress={takePhoto}>
+              <FontAwesome name="camera" size={24} color="#bdbdbd" />
+            </TouchableOpacity>
+          </View>
+        </Camera>
+
+        <Text style={styles.photoLoad}>Завантажте фото</Text>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+          <TextInput
+            value={name}
+            onChangeText={setName}
+            style={styles.nameInput}
+            placeholder="Назва..."
+            placeholderTextColor="#bdbdbd"
+          />
+
+          <View style={styles.locationInputWrap}>
+            <TextInput
+              value={nameLocation}
+              onChangeText={setNameLocation}
+              style={styles.locationInput}
+              placeholder="Місцевість..."
+              placeholderTextColor="#bdbdbd"
+            />
+            <Feather
+              name="map-pin"
+              size={24}
+              color="#bdbdbd"
+              style={styles.locationIcon}
+            />
+          </View>
+        </KeyboardAvoidingView>
+        <TouchableOpacity
+          style={[styles.publishBtn, pablishBtnIsActive]}
+          onPress={onPublish}
+        >
+          <Text style={[styles.textPublishBtn, textPublishBtnIsActive]}>
+            Опублікувати
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    paddingHorizontal: 25,
+    paddingTop: 10,
+    backgroundColor: '#fff',
+  },
+  camera: {
+    borderRadius: 8,
+    height: 180,
+    width: 288,
+    marginBottom: 5,
+    alignSelf: 'center',
+  },
+  photoView: {
+    position: 'relative',
+    flex: 1,
+    width: 288,
+    height: 180,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
+  takePhotoWrap: {
+    width: 60,
+    height: 60,
+    borderRadius: 50,
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  photoLoad: {
+    color: '#bdbdbd',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  nameInput: {
+    marginTop: 10,
+    paddingBottom: 7,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bdbdbd',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  locationInputWrap: {
+    position: 'relative',
+    marginTop: 10,
+  },
+  locationInput: {
+    paddingLeft: 28,
+    paddingBottom: 7,
+    borderBottomWidth: 1,
+    borderBottomColor: '#bdbdbd',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  locationIcon: {
+    position: 'absolute',
+    top: -3,
+  },
+  publishBtn: {
+    height: 40,
+    marginTop: 20,
+    marginBottom: 16,
+    alignItems: 'center',
+    backgroundColor: '#f6f6f6',
+    paddingVertical: 6,
+    paddingHorizontal: 70,
+    borderRadius: 50,
+  },
+  publishBtnIsActive: {
+    backgroundColor: '#ff6c00',
+  },
+  textPublishBtn: {
+    color: '#bdbdbd',
+    fontSize: 16,
+    lineHeight: 19,
+  },
+  textPublishBtnIsActive: {
+    color: '#fff',
+  },
+  flipContainer: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
+    flex: 1,
+  },
+});
