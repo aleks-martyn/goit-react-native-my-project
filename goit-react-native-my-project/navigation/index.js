dd9373aca0a3824d56c6ecccbaec3b009de0c59a
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-08-06 16:10:24 +0300

    Delete index.js

--- goit-react-native-my-project/navigation/index.js
+++
@@ -1,9 +0,0 @@
-import { useAuthentication } from '../utils/hooks/useAuthentication';
-import UserStack from './userStack';
-import AuthStack from './authStack';
-
-export default function RootNavigation() {
-  const { user } = useAuthentication();
-
-  return user ? <UserStack /> : <AuthStack />;
-}
